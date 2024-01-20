5aeabcd23798e5f362e2d060a7c43ce8c7e4bb41
Yuka Suzuki <yuka.kurihara821@gmail.com>
2024-01-20 10:13:59 +0000

    add navlink hover effect, add preloader

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
+  $(window).on('load', function () {
+    $('.loader-container').fadeOut(1000, function () {
+      $(this).css('display', 'none');
+    });
+  });
+
   $(window).on('scroll', () => {
     const scroll = $(window).scrollTop();
     if (scroll >= 50) {
