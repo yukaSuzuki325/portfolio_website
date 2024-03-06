6a1867b7d2f5ec0834a2a2a9599cc182de916102
Yuka Suzuki <yuka.kurihara821@gmail.com>
2024-03-06 10:37:10 +0000

    Implement AJAX and thankYouModal

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-$(document).ready(() => {
-  AOS.init();
-
-  $(window).on('load', function () {
-    $('.preloader-container').fadeOut(3500, function () {
-      // console.log('here');
-      $(this).css('display', 'none');
-    });
-  });
-
-  $(window).on('scroll', () => {
-    const scroll = $(window).scrollTop();
-    if (scroll >= 50) {
-      $('.sticky').addClass('stickyadd');
-    } else {
-      $('.sticky').removeClass('stickyadd');
-    }
+$(window).on('load', function () {
+  $('.preloader-container').fadeOut(3500, function () {
+    // console.log('here');
+    $(this).css('display', 'none');
   });
 });
+
+$(window).on('scroll', () => {
+  const scroll = $(window).scrollTop();
+  if (scroll >= 50) {
+    $('.sticky').addClass('stickyadd');
+  } else {
+    $('.sticky').removeClass('stickyadd');
+  }
+});
+
+AOS.init();
 
 $('.portfolio-img').hover(
   function () {
@@ -31,3 +29,24 @@ $('.portfolio-img').hover(
     });
   }
 );
+
+$('#contact-form').submit(function (e) {
+  e.preventDefault();
+
+  var formData = $(this).serialize();
+
+  $.ajax({
+    type: 'POST',
+    url: 'php/sendmail.php',
+    data: formData,
+    success: function (response) {
+      var myModal = new bootstrap.Modal(
+        document.getElementById('thankYouModal')
+      );
+      myModal.show();
+    },
+    error: function () {
+      alert('There was an error sending the message.');
+    },
+  });
+});
